b9b10febf2566faf658b5b9daf0946ddeaaa25ca
Ian Shepherd <me@ianshepherd.dev>
2020-08-16 21:26:36 -0600

    dist

---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis nihil
+      ducimus a sapiente maiores, corrupti ipsum. Tenetur dicta, minus et vel
+      earum sequi quas illo cum culpa nulla illum dolore suscipit atque, porro
+      laboriosam repudiandae vero veritatis, rem aut natus numquam reiciendis!
+      Iusto ipsum beatae saepe minus cupiditate ab alias.
+    </p>
+  </div>
+</template>
